e65314af73c61e7ad9c43e42e3eac8ca5675485e
Andrii Biriev <a@konservs.com>
2016-11-01 14:23:30 +0200

    Added default config file.

---
+++ config/config.local.php
@@ -0,0 +1,39 @@
+<?php
+//Caching type
+define('CACHE_TYPE','files');
+//Debug mode
+define('DEBUG_MODE','1');
+//Enable / disable site indexing
+define('DEBUG_SITENOINDEX','1');
+//Cache HTML pre-rendered blocks
+define('DEBUG_PAGES_CACHE','1');
+
+//Google Analytics ID
+define('GOOGLE_ANALYTICS_ID','');
+//Database host
+define('MYSQL_DB_HOST','127.0.0.1');
+//Database username
+define('MYSQL_DB_USERNAME','brilliant');
+//Database password
+define('MYSQL_DB_PASSWORD','');
+//Database
+define('MYSQL_DB_NAME','root');
+
+//Email sender
+define('EMAIL_SEND_EFROM','noreply@example.com');
+//Email sender
+define('EMAIL_SEND_NFROM','Brilliant ROBOT');
+
+//Default hostname
+define('BHOSTNAME','brilliant.tv');
+
+//
+define('MEDIA_PATH_ORIGINAL','/var/www/');
+define('MEDIA_PATH_RESIZED','/var/www/');
+define('MEDIA_URL','http://media.brilliant.tv');
+define('MEDIA_TRUSTED_IMAGE_SIZES','r185x115');
+define('WATERMARK_PATH','/var/www/watermark.png');
+define('WATERMARK_POSITION','1');
+define('WATERMARK_MINWIDTH',100);
+define('WATERMARK_MINHEIGHT',100);
+

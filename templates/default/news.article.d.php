1646b8e50b6b5d7c66d90bc12d76c38942d311a2
Andrii Biriev <a@konservs.com>
2016-11-03 11:33:33 +0200

    Added article template

---
+++ templates/default/news.article.d.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * One article page
+ *
+ * @author Andrii Biriev, <a@konservs.com>
+ * @copyright © Andrii Biriev, <a@konservs.com>
+ */
+defined('BEXEC')or die('NO direct access');
+?>
+<div id="news-article">
+
+</div>

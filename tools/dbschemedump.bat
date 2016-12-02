0a8b06a836d2463ad757f1d08e37bad384397d6a
Andrii Biriev <a@konservs.com>
2016-12-02 15:24:42 +0200

    Migrations maker

--- tools/dbschemedump.bat
+++ tools/dbschemedump.bat
@@ -1,9 +1,9 @@
 @echo off
 rem -------------------------------------------------------------
-rem  Brilliant Framework command line tools script for Windows.
+rem  Brilliant migrations maker
 rem -------------------------------------------------------------
 @setlocal
 set BRILLIANT_PATH=%~dp0
 if "%PHP_COMMAND%" == "" set PHP_COMMAND=php.exe
-"%PHP_COMMAND%" "%BRILLIANT_PATH%brilliant" %*
+"%PHP_COMMAND%" "%BRILLIANT_PATH%dbschemedump" %*
 @endlocal

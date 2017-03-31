7eda366ec7f8905aa68a7fdc26d2761cb3bbf36b
konserv <a@konservs.com>
2017-03-31 14:27:46 +0300

    Created BLoggerFile to logging to the file

--- libraries/Log/BLoggerFile.php
+++ libraries/Log/BLoggerFile.php
@@ -11,12 +11,38 @@ use Brilliant\log\BLogger;
 class BLoggerFile extends BLogger{
 	public $logsDirectory;
 	protected $logFile='';
-	public function 
+	/**
+	 */
+	public function getLogFile(){
+		if(empty($this->logFile)){
+			$now = new \DateTime();
+			$this->logFile = $this->logsDirectory.'log_'.$now->format('Y_m_d_h_i_s').'.log';
+			}
+		return $this->logFile;
+		}
 	/**
 	 *
 	 */
 	public function addToLog($msg,$level=LL_GENERAL){
-		echo('[dd.mm.YYYY hh:mm:ss] '.$msg.PHP_EOL);
+		$now = new \DateTime();
+		$pref=$now->format('[Y-m-d h:i:s]');
+		switch($level){
+			case LL_GENERAL:
+				$pref .= ' [-] ';
+				break;
+			case LL_ERROR:
+				$pref .= ' [E] ';
+				break;
+			case LL_WARNING:
+				$pref .= ' [W] ';
+				break;
+			case LL_DEBUG:
+				$pref .= ' [D] ';
+				break;
+			default:
+				$pref .= ' [?] ';
+			}
+		file_put_contents($this->getLogFile(),$pref.$msg.PHP_EOL,FILE_APPEND | LOCK_EX);
 		}
 	/**
 	 *
@@ -24,5 +50,4 @@ class BLoggerFile extends BLogger{
 	public function addHR(){
 		$this->addToLog('---------------------------------------------------------');
 		}
-
 	}

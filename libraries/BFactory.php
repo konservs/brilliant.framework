0823f847bfa1671cc02c0c01aeca3509728327ec
konserv <a@konservs.com>
2017-04-04 12:39:29 +0300

    BFactory documentation

--- libraries/BFactory.php
+++ libraries/BFactory.php
@@ -1,56 +1,63 @@
 <?php
 /**
  * Brilliant Framework factory
- * 
+ *
  * @author Andrii Biriev
- * 
+ *
  * @copyright © Andrii Biriev, <a@konservs.com>
  */
 namespace Brilliant;
 
-use Brilliant\log\BLog;
-use Brilliant\sql\BMySQL;
-use Brilliant\cache\BCache;
+use Brilliant\Log\BLog;
+use Brilliant\Sql\BMySQL;
+use Brilliant\Cache\BCache;
 
-class BFactory{
-	protected static $db=NULL;
+class BFactory {
+	protected static $db = NULL;
+
 	/**
+	 * Get BMySQL instance
 	 *
+	 * @return BMySQL|null
 	 */
-	public static function getDBO(){
-		if(!empty(self::$db)){
+	public static function getDBO() {
+		if (!empty(self::$db)) {
 			return self::$db;
-			}
-		BLog::addToLog('[BFactory] Connecting to the database "'.MYSQL_DB_HOST.'"...');
-		self::$db=BMySQL::getInstanceAndConnect();
-		if(empty(self::$db)){
-			BLog::addToLog('[BFactory] Could not connect to the MySQL database!',LL_ERROR);
+		}
+		BLog::addToLog('[BFactory] Connecting to the database "' . MYSQL_DB_HOST . '"...');
+		self::$db = BMySQL::getInstanceAndConnect();
+		if (empty(self::$db)) {
+			BLog::addToLog('[BFactory] Could not connect to the MySQL database!', LL_ERROR);
 			return NULL;
-			}
+		}
 		return self::$db;
-		}
-	/**
-	 *
-	 */
-	public static function getCache(){
-		if(CACHE_TYPE){
-			$bcache=BCache::getInstance();
-			return $bcache;
-			}
-		return NULL;
-		}
+	}
 
 	/**
-	 *
+	 * get BCache
+	 * @return BCache|null
 	 */
-	public static function getTempFn(){
-		$tempfn=BROOTPATH.'temp'.DIRECTORY_SEPARATOR;
+	public static function getCache() {
+		if (CACHE_TYPE) {
+			$bCache = BCache::getInstance();
+			return $bCache;
+		}
+		return NULL;
+	}
+
+	/**
+	 * Get temporary filename
+	 *
+	 * @return string
+	 */
+	public static function getTempFn() {
+		$tempFileName = BROOTPATH . 'temp' . DIRECTORY_SEPARATOR;
 		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 		$charactersLength = strlen($characters);
-		for ($i=0;$i<15;$i++){
-			$tempfn.= $characters[rand(0, $charactersLength - 1)];
-			}
-		$tempfn.='.tmp';
-		return $tempfn;
+		for ($i = 0; $i < 15; $i++) {
+			$tempFileName .= $characters[rand(0, $charactersLength - 1)];
 		}
+		$tempFileName .= '.tmp';
+		return $tempFileName;
 	}
+}

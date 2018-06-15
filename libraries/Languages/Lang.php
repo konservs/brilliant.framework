9ad583edf73f4918e61f461cc08a08c4fc537558
Andrii Biriev <a@konservs.com>
2018-06-15 18:45:03 +0300

    Draft for Lang

--- libraries/Languages/Lang.php
+++ libraries/Languages/Lang.php
@@ -4,13 +4,20 @@ use \Brilliant\Languages\Storage;
 
 class Lang{
 	$storageDefault;
-	//To take English if mail language does not have such translation
+	//To take English if main language does not have such translation
 	$storageBackup;
 
 	public static function init($languageCode){
 	}
-	public static function storagePreload(){
+	public static function getStorage(){
+	}
+	public static function storagePreload($languageCode){
+		$storage = self::getStorage();
+		$storage->init($languageCode);
 	}
 	public static function loadFromStorage(){
+	}
+	public status function _($languageConstant){
+		$storage->translate($languageConstant);
 	}
 }
---
+++ libraries/Languages/Storage/Abstract.php
@@ -0,0 +1,5 @@
+<?php
+
+class Abstract{
+	$cacheInMemory = true;
+	}

95f03f1917f746fee98bea8a906ba0588c87762d
konserv <a@konservs.com>
2018-12-26 14:27:28 +0200

    Brilliant MVC manager - added extensions manager

---
+++ libraries/MVC/BMVCManager.php
@@ -0,0 +1,22 @@
+<?php
+namespace Brilliant\MVC;
+
+/**
+ * Basic class to control list of installed components.
+ * 
+ * @author Andrii Biriev <a@konservs.com>
+ */
+class BMVCManager{
+	/**
+	 * Executed during initializing
+	 */
+	public static function init(){
+		
+	}
+	/**
+	 *
+	 */
+	public static function componentPathAdd($path){
+		
+	}
+}
